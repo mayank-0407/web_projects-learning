105e3bb0580a768a2ae410645bc7357bc8d2d3db
mayank-0407 <mayankdatabase04@gmail.com>
2023-02-04 16:59:12 +0530

    Solving errors in signup

--- tictac/server.js
+++ tictac/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyparser = require("body-parser");
 var database = require('./database');
+var router = express.Router();
 
 const app = express()
 const port = 3000
@@ -13,27 +14,51 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body)
-    var user_email = req.body.email;
+    var user_email = req.body.Email;
     var user_fname = req.body.fname;
     var user_lname = req.body.lname;
     var user_pass = req.body.password;
     var user_pass2 = req.body.cpassword;
 
-    query = `
-    INSERT INTO table_name (fname, lname, email, password) VALUES (user_fname, user_lname, user_email,user_pass );
+    console.log(user_email)
+
+    var query = `
+    INSERT INTO logintictac (id,fname, lname, email, password) VALUES (2,user_fname,user_lname,user_email,user_pass);
     `
-    database.query(query, function(error, data) {
-        if (data.length > 0) {
-            for (var count = 0; count < data.length; count++) {
-                // request.session.user_id = data[count].user_id;
-                response.redirect("/");
-            }
+    database.query(query, [user_fname, user_lname, user_email, user_pass], function(error, data,field) {
+        if (error) {
+            console.error(error);
+            res.send("An error occurred");
         } else {
-            response.send("INCORRECT VALUE")
+            console.error(data);
+            // res.send("SUCCESS");
         }
-    })
-    res.send('POST FILE SUBMITTED')
-})
+    });
+    res.send('Successfully signed in')
+});
+// app.post('/', (req, res) => {
+//     console.log(req.body)
+//     var user_email = req.body.email;
+//     var user_fname = req.body.fname;
+//     var user_lname = req.body.lname;
+//     var user_pass = req.body.password;
+//     var user_pass2 = req.body.cpassword;
+
+//     query = `
+//     INSERT INTO table_name (fname, lname, email, password) VALUES (user_fname, user_lname, user_email,user_pass );
+//     `
+//     database.query(query, function(error, data) {
+//         if (data.length > 0) {
+//             for (var count = 0; count < data.length; count++) {
+//                 // request.session.user_id = data[count].user_id;
+//                 response.redirect("/");
+//             }
+//         } else {
+//             response.send("INCORRECT VALUE")
+//         }
+//     })
+//     res.send('POST FILE SUBMITTED')
+// })
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })
